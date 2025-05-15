e31066d6877f960ea32c0237421834fc881ca696
Gabriel Gomes <gggabriel368@gmail.com>
2025-05-15 15:37:10 -0300

    estrutura de pastas completa

---
+++ Front-End/src/components/button.jsx
@@ -0,0 +1,7 @@
+function Button (){
+    return (
+        <button>CLICA AI ZÉ</button>
+    )
+}
+
+export default Button
---
+++ Front-End/src/containers/home/index.jsx
@@ -0,0 +1,12 @@
+import Button from "../../components/button"
+
+function Home (){
+    return (
+        <div>
+            ola mundo
+            <Button></Button>
+        </div>
+    )
+}
+
+export default Home
