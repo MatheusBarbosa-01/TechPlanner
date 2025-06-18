af1241adb4610eaffb903eff348ffd3f0a5567db
Gabriel Gomes <gggabriel368@gmail.com>
2025-06-18 20:30:59 -0300

    arrumando o login

--- Front-End/src/containers/login/index.jsx
+++ Front-End/src/containers/login/index.jsx
@@ -1,23 +1,43 @@
 import Button from "../../components/button";
 import './style.css'
+import './index'
+import api from '../../services/api.json'
 
 function Login (){
+    const matricula = document.querySelector('#matricula')
+
+    async function send(e){
+        e.preventDefault()
+
+        api.Alunos.forEach( v => {
+            if(v.matrícula === matricula.value){
+                alert("aluno")
+            }
+        })
+        api.Professor.forEach( v => {
+            if(v.matrícula === matricula.value){
+                alert("professor")
+            }
+        })
+    }
+
     return (
         <div id="login">
- <div id="modalLogin">
-            <div id="blueModal">
-                <p>Olá!</p><br/>
-                <p>Coloque as informações para entrar em sua conta</p>
-            </div>
-            <div id="whiteModal">
-                <h1>ENTRE AQUI</h1>
-                <input type="text" placeholder="Digite sua matrícula"/>
-                <input type="password" placeholder="Digite sua senha"/>
-                <Button></Button>
-            </div>
+            <form onSubmit={send}>
+                <div id="modalLogin">
+                    <div id="blueModal">
+                        <p>Olá!</p><br/>
+                        <p>Coloque as informações para entrar em sua conta</p>
+                    </div>
+                    <div id="whiteModal">
+                        <h1>ENTRE AQUI</h1>
+                        <input id="matricula" type="text" placeholder="Digite sua matrícula"/>
+                        <input id="senha" type="password" placeholder="Digite sua senha"/>
+                        <Button></Button>
+                    </div>
+                </div>
+            </form>
         </div>
-        </div>
-       
     )
 }
 
--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -2,7 +2,7 @@ import { Provider } from "./components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './containers/home/index'
-import Login from "./containers/login"
+import Login from "./containers/login/index"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
